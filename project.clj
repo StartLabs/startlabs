e375740efe272fff797b9818b90752a7f33b07cb
Ethan Sherbondy <sherbondye@gmail.com>
2012-07-23 00:30:41 -0700

    initial schema for users

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "The new Start Labs Pad"
   :url "http://startlabs.org"
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/data.json "0.1.2"]
                  [noir-cljs "0.3.4"]
                  [jayq "0.1.0-alpha4"]
                  [fetch "0.1.0-alpha2"]
---
+++ src/startlabs/models/json_dtm.clj
@@ -0,0 +1,18 @@
+(ns startlabs.models.json-dtm
+  (:require [clojure.data.json :as json]
+            [clojure.pprint :as pp]))
+
+(defn get-sample-data []
+  (json/read-json (slurp "src/startlabs/models/userinfo_example.json")))
+
+(defn datalogify [data ns]
+  (letfn [(dtm-kv [k v]
+            {
+              :db/id "#db/id[:db.part/db]"
+              :db/ident (keyword (str (name ns) "/" (name k)))
+              :db/valueType :db.type/string
+              :db/cardinality :db.cardinality/one
+              :db/doc (str "Example value: " v)
+              :db.install/_attribute :db.part/db
+            })]
+    (pp/pprint (apply vector (map dtm-kv (keys data) (vals data))))))
