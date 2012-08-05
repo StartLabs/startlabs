3a704192d48a90ca74eb713f6700c8d0fd0523eb
Ethan Sherbondy <sherbondye@gmail.com>
2012-08-05 16:09:27 -0700

    adding jquery 1.7.2

--- project.clj
+++ project.clj
@@ -7,14 +7,22 @@
                   [noir "1.3.0-beta10"]
                   [clj-http "0.5.2"]
                   [oauth-clj "0.0.5"]
+                  [com.datomic/datomic-free "0.8.3372" 
+                   :exclusions [org.slf4j/slf4j-nop org.slf4j/log4j-over-slf4j]]
+                  [org.slf4j/slf4j-log4j12 "1.6.4"]
                   ;cljs
                   [com.keminglabs/c2 "0.2.0"]
                   [jayq "0.1.0-alpha4"]
                 ]
+  ; download datomic here: http://downloads.datomic.com/free.html
+  ; put it into datomic-free, then install locally with mvn
+  :repositories {"local" ~(str (.toURI (java.io.File. "datomic-free")))}
+
   :source-paths ["src/clj"]
   :cljsbuild {:builds
               [{:source-path "src/cljs"
                 :compiler {:output-to "resources/public/client.js"
+                           :externs ["resources/externs/jquery.js"]
                            :pretty-print false
                            :optimizations :simple}}]}
                            ; switch to :advanced for production
