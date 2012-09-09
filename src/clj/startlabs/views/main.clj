6ac67c3b111a2aa06b82b0c026b5183d2cc4747e
Ethan Sherbondy <sherbondye@gmail.com>
2012-09-09 03:46:06 -0400

    bootcamp div.

--- src/clj/startlabs/views/main.clj
+++ src/clj/startlabs/views/main.clj
@@ -7,12 +7,34 @@
 (defpage "/" []
   (common/layout
     [:h1.slug "Interested in " [:strong "Startups"] "?"]
-    [:p "You've come to the right place."]))
+    [:div.row-fluid
+      [:div.span6
+        [:h2 "You've come to the right place."]
+        [:p [:strong "StartLabs"] " was established by MIT engineering students "
+            "in the summer of 2011 to spread entrepreneurship."
+            "We support and encourage students to:"]
+            [:ol
+              [:li "Start their own companies – transforming ideas and class "
+                   "projects into seed-stage ventures."]
+              [:li "Work in rapidly expanding companies – place students in internships "
+                   "and full-time positions at promising startups."]]
+            [:p "We are creating the next generation of technical entrepreneurs."]]
+      [:div.span6
+        [:h2 "Upcoming Events"]
+        [:p "On Monday, October 8th, we'll be hosting " [:strong "Startup Bootcamp"] ", "
+            "a free one-day event featuring talks by startup founders."]
+        [:p "It's from 9am to 5pm in Kresge Auditorium."]
+
+        [:div
+          [:a.center.bootcamp {:href "http://startupbootcamp.mit.edu"}
+            [:img {:src "/img/bootcamp.png" :alt "Startup Bootcamp" :width "240px"}]]]]
+      ]))
 
 (defpage "/about" []
   (common/layout
     [:h1 "About Us"]
     [:div
+      [:p "Here's some lousy copy from the old website:"]
       [:p "StartLabs is a non-profit created out of the ideals of collegiate entrepreneurship."]
       [:p "The goal of StartLabs is to catalyze student engineers to bring technical innovations 
            to society through entrepreneurship, specifically by having students:"]
