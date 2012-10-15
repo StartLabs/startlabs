c9a0eb2e2c098a937a90191657e095434abf19fd
Ethan Sherbondy <sherbondye@gmail.com>
2012-10-15 17:58:20 -0400

    adding setup scripts. Need to document in readme.

---
+++ setup/mac.sh
@@ -0,0 +1,36 @@
+echo Installing all of the junk. Hope this works.
+echo Grabbing git submodules
+git submodule init && git submodule update
+
+if ! type "lein" > /dev/null 2>&1; then
+  echo Installing lein to /usr/local/bin
+  curl https://raw.github.com/technomancy/leiningen/preview/bin/lein > lein
+  chmod +x lein
+  mv lein /usr/local/bin
+else
+  echo Lein already installed, skipping.
+fi
+
+if ! type "brew" > /dev/null 2>&1; then
+  echo Installing brew
+  ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+else
+  echo Homebrew already installed, skipping.
+fi
+
+echo Compiling cljx files. Give it a second.
+lein cljx
+
+if ! type "node" > /dev/null 2>&1; then
+  echo Installing node and npm
+  brew install node
+  curl http://npmjs.org/install.sh | sh
+else
+  echo Node already installed, skipping.
+fi
+
+echo Installing npm packages for bootstrap
+sudo npm install recess connect uglify-js jshint -g
+
+
+# still need to do nice .lein-env bit
--- start.sh
+++
@@ -1,3 +0,0 @@
-curl https://raw.github.com/technomancy/leiningen/preview/bin/lein > lein
-chmod +x lein
-mv lein /usr/local/bin
