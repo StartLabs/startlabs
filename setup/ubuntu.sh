151866b993e46c72c50b468522088e97204f4be6
Ethan Sherbondy <ethan@startlabs.org>
2013-01-02 23:53:21 +0000

    ubuntu script now includes datomic free setup.

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -1,10 +1,11 @@
+#! /bin/zsh
 # run this after installing git and cloning startlabs
 # sudo apt-get install git;
 # git clone https://github.com/sherbondy/startlabs
 
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
-sudo apt-get install nginx openjdk-7-jre-headless make python-software-properties nodejs npm zsh daemontools wget
+sudo apt-get install nginx openjdk-7-jre-headless make python-software-properties nodejs npm zsh daemontools wget unzip
 
 # make the nginx log directory
 sudo mkdir /etc/nginx/logs
@@ -24,6 +25,11 @@ cd ..
 git submodule init && git submodule update
 sudo npm install recess connect uglify-js jshint -g
 cd bootstrap; make bootstrap
+
+git clone https://github.com/cldwalker/datomic-free.git ~/.datomic-free
+echo 'alias datomic-free=$HOME/.datomic-free/bin/datomic-free' >> ~/.zshrc
+source ~/.zshrc
+datomic-free update
 
 # now all that's left to do is configure datomic and .lein-env.
 # I should automate this eventually.
