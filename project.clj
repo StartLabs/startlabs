cf041df2f2dc94dfd5813bd366799249a97eda73
Ethan Sherbondy <sherbondye@gmail.com>
2013-01-14 19:48:30 -0500

    updated package versions without a hitch.

--- project.clj
+++ project.clj
@@ -7,33 +7,33 @@
 
   :dependencies [ ;clj core
                  [org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.json "0.2.0"]
+                 [org.clojure/data.json "0.2.1"]
                  [org.clojure/tools.logging "0.2.4"]
                  [org.clojure/core.incubator "0.1.2"] ; for -?> goodness
                  [org.clojure/math.numeric-tower "0.0.2"]
                  
                  ;clj other
-                 [compojure "1.1.3"]
+                 [compojure "1.1.5"]
                  [hiccup "1.0.2"]
-                 [lib-noir "0.2.0"]
+                 [lib-noir "0.3.4"]
                  [org.clojars.cjschroed/sandbar "0.4.0"]
 
                  [clj-http "0.6.3"]
-                 [oauth-clj "0.1.0"]
-                 [com.datomic/datomic-free "0.8.3692"
+                 [oauth-clj "0.1.1"]
+                 [com.datomic/datomic-free "0.8.3731"
                   :exclusions [org.slf4j/slf4j-nop org.slf4j/log4j-over-slf4j]]
-                 [org.slf4j/slf4j-log4j12 "1.6.6"]
-                 [environ "0.3.0"]
+                 [org.slf4j/slf4j-log4j12 "1.7.2"]
+                 [environ "0.3.1"]
                  [clj-aws-s3 "0.3.3"]
-                 [markdown-clj "0.9.12"]
+                 [markdown-clj "0.9.18"]
                  [clj-time "0.4.4"]
-                 [com.draines/postal "1.9.1"]
+                 [com.draines/postal "1.9.2"]
                  [climp "0.1.2"]
                  [sherbondy/clj-stripe "1.0.4"]
 
                  ;cljs
                  [crate "0.2.3"]
-                 [jayq "0.3.2"]
+                 [jayq "2.0.0"]
                 ]
 
   :source-paths ["src/clj" ".generated/clj"]
